6c16a613c47f3ee33bf475f42b5e406578fed7c9
Matz Hilven <hilvenmatz@gmail.com>
2023-11-27 19:20:27 +0100

    feat: day 0

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-mod days;
-
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
+mod days;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(days::zero::hello_world);
     };
 
     Ok(config.into())
