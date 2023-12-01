6d2d9180cdf02a59b1e5df87d235cf06432a8ff6
Matz Hilven <hilvenmatz@gmail.com>
2023-12-01 15:04:10 +0100

    chore: format

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,10 +1,7 @@
-
 pub fn configure(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.configure(zero::configure);
     cfg.configure(one::configure);
 }
 
-pub mod zero;
 pub mod one;
-
-
+pub mod zero;
--- src/days/one/mod.rs
+++ src/days/one/mod.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, HttpResponse, Responder, web};
+use actix_web::{get, web, HttpResponse, Responder};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(part_1_and_2);
@@ -6,21 +6,20 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
 
 #[get("/1/{nums}*")]
 async fn part_1_and_2(nums: web::Path<String>) -> impl Responder {
-    let nums = nums.split('/').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
+    let nums = nums
+        .split('/')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
     let result = nums.iter().fold(0, |acc, &num| acc ^ num).pow(3);
     HttpResponse::Ok().body(format!("{}", result))
 }
 
-
-
 #[cfg(test)]
 mod tests {
-    use actix_web::{App, body, test};
+    use actix_web::{body, test, App};
     #[actix_web::test]
     async fn test_part_1() {
-        let app = test::init_service(
-            App::new().configure(super::configure)
-        ).await;
+        let app = test::init_service(App::new().configure(super::configure)).await;
         let req = test::TestRequest::get().uri("/1/4/8").to_request();
         let resp = test::call_service(&app, req).await;
 
@@ -33,9 +32,7 @@ mod tests {
 
     #[actix_web::test]
     async fn test_part_2_1() {
-        let app = test::init_service(
-            App::new().configure(super::configure)
-        ).await;
+        let app = test::init_service(App::new().configure(super::configure)).await;
         let req = test::TestRequest::get().uri("/1/10").to_request();
         let resp = test::call_service(&app, req).await;
 
@@ -48,9 +45,7 @@ mod tests {
 
     #[actix_web::test]
     async fn test_part_2_2() {
-        let app = test::init_service(
-            App::new().configure(super::configure)
-        ).await;
+        let app = test::init_service(App::new().configure(super::configure)).await;
         let req = test::TestRequest::get().uri("/1/4/5/8/10").to_request();
         let resp = test::call_service(&app, req).await;
 
